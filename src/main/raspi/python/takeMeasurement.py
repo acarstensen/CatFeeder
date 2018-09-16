202e875b6a1aea81cf7f133dafe92c36e16a0ecf
Adam Carstensen <acarstensen@users.noreply.github.com>
2018-09-16 07:29:08 -0500

    average amount remaining update

--- src/main/raspi/python/takeMeasurement.py
+++ src/main/raspi/python/takeMeasurement.py
@@ -16,29 +16,67 @@ GPIO.setwarnings(False)
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-# send a quick sound wave
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+# figure out max valid reading
+maxValidDistance = 9.0
+if foodOrWater == "Water":
+  maxValidDistance = 7.9
+print "Using maxValidDistance: " + str(maxValidDistance)
 
-# measure the time it takes to bounce
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+# take 20 measurements so you can calculate a nice average
+readings = []
+for i in range(0, 20):
+    # send a quick sound wave
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+    # measure the time it takes to bounce
+    while GPIO.input(ECHO)==0:
+      pulse_start = time.time()
 
-# calculate distance
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 6751.95
-distance = round(distance, 2)
+    while GPIO.input(ECHO)==1:
+      pulse_end = time.time()
 
-# output distance
-print "Distance to " + foodOrWater + ": " + str(distance) + " inches"
+    # calculate distance
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 6751.95
+    distance = round(distance, 2)
+    print "Distance to " + foodOrWater + ": " + str(distance) + " inches"
 
+    # calculate amount remaining
+    amountRemaining = maxValidDistance - distance
+    print "Amount of " + foodOrWater + " remaining: " + str(amountRemaining) + " inches"
+
+    # append to list
+    readings.append(amountRemaining)
+
+# sort the list
+print "unsorted: " + str(readings)
+readings.sort()
+print "sorted: " + str(readings)
+
+# remove the first and last 3 to get rid of outliers
+del readings[19]
+del readings[18]
+del readings[17]
+del readings[0]
+del readings[0]
+del readings[0]
+print "after removal: " + str(readings)
+
+# calculate average
+avgAmountRemaining = float(sum(readings)) / max(len(readings), 1)
+print "Average amount of " + foodOrWater + " remaining: " + str(avgAmountRemaining) + " inches"
+
+# if less than 0, 0
+if avgAmountRemaining < 0:
+  avgAmountRemaining = 0
+  print "Rounding up negative number to 0."
+
+# print to log file
 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H%M%S')
 fileName = "/home/pi/catfeeder/logs/measure" + foodOrWater + "_" + timestamp + ".log"
-fileContents = str(distance) + " inches"
+fileContents = str(avgAmountRemaining) + " inches"
 f = open(fileName,'w')
 f.write(fileContents)
 f.close()
